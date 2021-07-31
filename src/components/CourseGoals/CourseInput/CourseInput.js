b85de811fe51fd192d152640a03e0150658e108c
Thiago Costa <thiagohrcosta86@gmail.com>
2021-07-31 13:27:48 -0300

    Refactoring invalid feedback code

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -8,6 +8,9 @@ const CourseInput = props => {
   const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = event => {
+    if(event.target.value.trim().length > 0){
+      setIsValid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
@@ -22,8 +25,8 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label style={{color: !isValid ? 'red' : 'black'}}>Course Goal</label>
+      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+        <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
